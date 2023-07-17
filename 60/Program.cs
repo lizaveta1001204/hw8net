e53a607752d76ce4900e7d59abe172826b2d595c
liza <lizaveta2011@tut.by>
2023-07-17 15:52:12 +0300

    60

---
+++ 60/Program.cs
@@ -0,0 +1,34 @@
+﻿int[,,] array3D = new int[2, 2, 2];
+FillArray3D(array3D);
+PrintIndex3D(array3D);
+
+void FillArray3D(int[,,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+void PrintIndex3D(int[,,] arr)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"{array3D[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
