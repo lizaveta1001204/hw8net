01d95d704ac31fdad5d56ae1b53290cb9d69ae8e
liza <lizaveta2011@tut.by>
2023-07-17 15:47:30 +0300

    58

---
+++ 58/Program.cs
@@ -0,0 +1,60 @@
+﻿int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+int[,] array = new int[rows, columns];
+int[,] secondArray = new int[rows, columns];
+int[,] resultArray = new int[rows, columns];
+FillArrayRandom2D(array);
+PrintArray2D(array);
+Console.WriteLine();
+FillArrayRandom2D(secondArray);
+PrintArray2D(secondArray);
+Console.WriteLine();
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArrayRandom2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+    if (array.GetLength(0) != secondArray.GetLength(1))
+    {
+        Console.WriteLine(" Нельзя перемножить ");
+        return;
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < secondArray.GetLength(1); j++)
+        {
+            resultArray[i, j] = 0;
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                resultArray[i, j] += array[i, k] * secondArray[k, j];
+            }
+        }
+    }
+
+
+PrintArray2D(resultArray);
